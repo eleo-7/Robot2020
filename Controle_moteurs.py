a88b5c266c3903a40b2cf9b2b578e793f07bf158
Eleonore Brants <eleonore@brants.be>
2019-11-17 14:17:52 +0100

    Update Controle_moteurs.py

--- Controle_moteurs.py
+++ Controle_moteurs.py
@@ -6,6 +6,7 @@ nb_pas_1_tour = 16*200
 
 def Tout_droit(mm,sens):#sens = Avant, not sens = arriere
 	nb_pas = int(nb_pas_1_tour/perimetre_roue)
+	print(nb_pas)
 	Comm_a.Write_to_arduino(f"moteur ttd;{nb_pas};{sens}")
 
 if __name__ == '__main__':
