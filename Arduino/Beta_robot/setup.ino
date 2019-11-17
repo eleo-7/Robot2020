56d9fab4645d40b9d77ca5b94ddd312014a903ee
Eleonore Brants <eleonore@brants.be>
2019-11-17 14:26:04 +0100

    dw

--- Arduino/Beta_robot/setup.ino
+++ Arduino/Beta_robot/setup.ino
@@ -1,3 +1,15 @@
 void setup() {
   Serial.begin(9600);
+
+  pinMode(stepPin,OUTPUT); 
+  pinMode(dirPin,OUTPUT);
+  pinMode(stepPin2,OUTPUT); 
+  pinMode(dirPin2,OUTPUT);
+  pinMode(MS1,OUTPUT);
+  pinMode(MS2,OUTPUT);
+  pinMode(MS3,OUTPUT);
+
+  digitalWrite(MS1,LOW);
+  digitalWrite(MS2,LOW);
+  digitalWrite(MS3,LOW);
   }
