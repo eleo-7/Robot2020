64f83b7958fd0a6b329e125308e1fadc7f71d74c
Eleonore Brants <eleonore@brants.be>
2019-11-17 14:19:03 +0100

    Update Controle_moteurs.py

--- Controle_moteurs.py
+++ Controle_moteurs.py
@@ -5,7 +5,7 @@ perimetre_roue = 216.7879893098 #mm
 nb_pas_1_tour = 16*200
 
 def Tout_droit(mm,sens):#sens = Avant, not sens = arriere
-	nb_pas = int(nb_pas_1_tour/perimetre_roue)
+	nb_pas = int((nb_pas_1_tour/perimetre_roue)*mm)
 	print(nb_pas)
 	Comm_a.Write_to_arduino(f"moteur ttd;{nb_pas};{sens}")
 
