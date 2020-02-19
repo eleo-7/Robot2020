fa72ecda4310b889586e045fb47799ba2f3d5574
Eleonore Brants <eleonore@brants.be>
2020-02-19 18:35:23 +0100

    Update Serveur_vers_app.py

--- Serveur_vers_app.py
+++ Serveur_vers_app.py
@@ -25,6 +25,10 @@ def avancer(g,d,sens):
     mot.Tout_droit(g,d)
     return(f"On avance de {g}cm,{d}cm")
 
+@app.route('/hello')
+def test():
+	return "hello"
+
 @app.route('/tourne/<int:deg>/int<sens>')#sens 1 = ccw, 0 = cw
 def tourner(deg,sens):
 	sens=bool(sens)
